0a2be2dd45740907d3f9741e219003c97ad5809e
Daniel Mane <danmane@gmail.com>
2013-11-25 01:47:20 -0600

    Factor a nice primes implementation out

--- Haskell/p46.hs
+++ Haskell/p46.hs
@@ -1,20 +1,10 @@
 import Data.List
+import Primes
 
-isPrime :: [Int] -> Int -> Bool
-isPrime plist n = all (\x -> n `mod` x /= 0) smallPrimes
-    where smallPrimes = takeWhile (\x -> x * x <= n) plist
-
-potentialPrimes = [6 * x + y | x <-[1..], y <- [-1,1]]
-primeList = 2:3:filter (isPrime primeList) potentialPrimes
-
-isPrimeNaive :: Int -> Bool
-isPrimeNaive n = all (/= 0)  (map (mod n) [2..n-1])
-naivePrimes = 2 : filter isPrimeNaive [3..]
-
-oddComposites = [3,5..] \\ primeList
+oddComposites = [3,5..] \\ primes
 doubleSquares = map ((*2) . (^2)) [1..]
 
-goldbachPredicate n = n `elem` [p + 2 * s | p <- filter (<n) primeList, s <- filter (<n) doubleSquares]
+goldbachPredicate n = n `elem` [p + 2 * s | p <- filter (<n) primes, s <- filter (<n) doubleSquares]
 
 main = print $ find (not . goldbachPredicate) oddComposites
---(print $ take 20 primeList) >> (print $ take 20 naivePrimes)
+--(print $ take 20 primes) >> (print $ take 20 naivePrimes)
--- Haskell/primes.hs
+++ Haskell/primes.hs
@@ -1,13 +1,11 @@
-import Data.List
-import qualified Data.Set as Set
+module Primes (primes) where
+isPrime :: [Int] -> Int -> Bool
+isPrime plist n = all (\x -> n `mod` x /= 0) smallPrimes
+    where smallPrimes = takeWhile (\x -> x * x <= n) plist
 
+potentialPrimes = [6 * x + y | x <-[1..], y <- [-1,1]]
+primes = 2:3:filter (isPrime primes) potentialPrimes
 
-primes :: [Int]
-primes = 2 : 3 : primesR [2,3] 5 where
-	primesR li n = case divisible n li of 
-		True  -> primesR li (n+2)
-		False -> n : primesR (n:li) (n+2)
-
-divisible :: Int -> [Int] -> Bool
-divisible x li = any (== 0) $ map (mod x) li
-
+isPrimeNaive :: Int -> Bool
+isPrimeNaive n = all (/= 0)  (map (mod n) [2..n-1])
+naivePrimes = 2 : filter isPrimeNaive [3..]
