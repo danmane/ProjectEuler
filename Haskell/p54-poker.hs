8efa8d35493d6a758eed6286b8b10223e76f8a65
Daniel Mane <danmane@gmail.com>
2014-04-23 23:04:54 -0700

    shorter parse

--- Haskell/p54-poker.hs
+++ Haskell/p54-poker.hs
@@ -34,24 +34,24 @@ getRankCounts :: Hand -> [(Int, Rank)]
 getRankCounts = getCounts . getRanks
 
 parse :: Hand -> PokerHand
-parse h = case (parseStraight h, parseFlush h) of
-  (Just c, Just _) -> StraightFlush c
-  (Just c, _)      -> Straight c
-  (_, Just c)      -> Flush c
-  _ -> case getRankCounts h of
-    [(4, r), (1, _)] -> FourKind r
-    [(3, r), (2, _)] -> FullHouse r
-    [(3, r1), (1, r2), (1, r3)] -> ThreeKind r1 r2 r3
-    [(2, r1), (2, r2), (1, r3)] -> TwoPair r1 r2 r3
-    [(2, r1), (1, r2), (1, r3), (1 r4)] -> Pair r1 r2 r3
+parse h
+  | isStraight h && isFlush h = StraightFlush maxRank
+  | isFlush h = Flush maxRank
+  | isStraight h = Straight maxRank
+  | otherwise = case getRankCounts h of
+    [(4, r), (1, _)]                              -> FourKind r
+    [(3, r), (2, _)]                              -> FullHouse r
+    [(3, r1), (1, r2), (1, r3)]                   -> ThreeKind r1 r2 r3
+    [(2, r1), (2, r2), (1, r3)]                   -> TwoPair r1 r2 r3
+    [(2, r1), (1, r2), (1, r3), (1 r4)]           -> Pair r1 r2 r3
     [(1, r1), (1, r2), (1, r3), (1, r4), (1, r5)] -> HighCard r1 r2 r3 r4 r5
   where
-    parseStraight h = if isStraight then Just $ Straight (last sorted) else Nothing
-    isStraight = [0..4] == map (+ (-smallest)) sorted
-    sorted@(smallest:_) = sort (getRanks h)
-
-    parseFlush h = if isFlush h then Just $ Flush (maximum (getRanks h)) else Nothing
+    isStraight = (== [0..4]) . uncurry map . (flip (-) . head &&& id)
+    sortedRanks = sort $ getRanks h
+    maxRank = last sortedRanks
     isFlush = (== 1) . length . nub . getSuits
+
+rankify :: PokerHand ->
 
 main :: IO ()
 main = print "hello"
